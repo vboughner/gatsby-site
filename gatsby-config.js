fcd5aab74c9a933fffa2707ecfff1c4e15b77505
Van Boughner <van.boughner@ntti3.io>
2018-02-27 16:43:16 -0800

    worked with tutorial on how to update layout

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,17 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: `Gatsby Default Starter`,
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-catch-links`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/src/markdown/`,
+        name: `markdown`,
+      },
+    },
+    `gatsby-transformer-remark`,
+  ],
 };
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,30 +4,30 @@ import Link from 'gatsby-link'
 const Header = () => (
   <div
     style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
+      background: '#f5f5f5',
+      marginBottom: '3rem',
+      borderBottom: '2px solid #e6e6e6',
     }}
   >
     <div
       style={{
         margin: '0 auto',
-        maxWidth: 960,
+        maxWidth: 980,
         padding: '1.45rem 1.0875rem',
       }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
+   >
+     <h1 style={{margin: 0, textAlign: 'center',fontSize: '18px'}}>
+        <Link to="/"
           style={{
-            color: 'white',
+            color: 'black',
             textDecoration: 'none',
           }}
         >
-          Gatsby
+          CodeStack
         </Link>
       </h1>
     </div>
   </div>
-)
+);
 
 export default Header
