d70e06428b63451b247c16bfdac851424c4d45f2
Van Boughner <van.boughner@ntti3.io>
2018-02-28 13:29:35 -0800

    experiments with the Netlify Identity widget for login

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -8,15 +8,20 @@ import Sidebar from '../components/Sidebar'
 import './index.css'
 import '../styles/layout-override.css'
 
+// was in Helmet
+// <!-- <script type="text/javascript" src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script> -->
+// was under header
+// <div data-netlify-identity-button>Login with Netlify Identity</div>
+
 const TemplateWrapper = ({ children }) => (
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title="Cloudwan API Documentation"
       meta={[
-        { name: "description", content: "Sample" },
-        { name: "keywords", content: "sample, something" }
-      ]}
-    />
+        { name: "description", content: "Descriptions of API for documentation of CLOUDWAN" },
+        { name: "keywords", content: "documentation, api" }
+      ]}>
+    </Helmet>
     <Header />
     <div
       style={{
